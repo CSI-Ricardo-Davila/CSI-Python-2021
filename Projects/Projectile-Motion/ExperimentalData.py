8efd6785bc9b80385cdeee7aecd40643617b1386
CSI-Ricardo-Davila <89204914+CSI-Ricardo-Davila@users.noreply.github.com>
2021-10-12 10:41:14 -0400

    Projectile Motion

---
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -0,0 +1,10 @@
+class ExperimentalData:
+    def __init__(self, gun:str, CartridgeCalibre:str, ammunition:str, ProjectileVelocity_mps:float, building:str, buildingHeight_m:float, gravity:float):
+        self.gun = gun
+        self.CartridgeCalibre = CartridgeCalibre
+        self.ammunition = ammunition
+        self.ProjectileVelocity_mps = ProjectileVelocity_mps
+        self.building = building
+        self.buildingHeight_m = buildingHeight_m
+        self.gravity = gravity
+    

f7b60b8f695c6dc244a098db7540b872da86e871
CSI-Ricardo-Davila <89204914+CSI-Ricardo-Davila@users.noreply.github.com>
2022-02-16 10:41:20 -0400

    hangman paso 2

--- Projects/Hangman/hangman.py
+++ Projects/Hangman/hangman.py
@@ -55,7 +55,33 @@ steps = ["""
 
 print(steps[3]) 
 
+
+invalid_caracters= ["!","@","~","`","#","$","%","^","&","*","(",")","-","_","+","=","[","]","{","}","|",":",";","'","<",",",".",">","/","?"]
+invalid_numbers = ["0","1","2","3","4","5","6","7","8","9"]
 def get_word():
     word_list = ["playa","oceano","patineta","zapato","tabla","generador","palma","computadora","televisor","pelicula","fotografia","comida","Patillas","Barceloneta","municipio","Vieques","gobernador","ventana","Aibonito","Culebra"]
-    return random.choice(word_list).lower()
+    return random.choice(word_list)
 
+def get_input():
+    
+    while(True):
+        letter = input("Chose your letter")
+
+        if(len(letter)!= 1):
+            print("error")
+            continue
+
+        # if((letter)== letter):
+        #     print("You already used this letter")
+
+        if(letter in invalid_numbers):
+            print("error")
+            continue
+
+        if(letter in invalid_caracters):
+            print("error")
+            continue
+
+        return letter
+
+print(get_input())

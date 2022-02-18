263a92b3ca6e0a7be9733f8219b50f86c4fda7b8
CSI-Ricardo-Davila <89204914+CSI-Ricardo-Davila@users.noreply.github.com>
2022-02-18 10:34:18 -0400

    Update hangman.py

--- Projects/Hangman/hangman.py
+++ Projects/Hangman/hangman.py
@@ -74,14 +74,19 @@ steps = ["""
          """
          ]
 
-print(steps[0]) 
+
 
 used_letters =[]
 invalid_caracters= ["!","@","~","`","#","$","%","^","&","*","(",")","-","_","+","=","[","]","{","}","|",":",";","'","<",",",".",">","/","?"]
 invalid_numbers = ["0","1","2","3","4","5","6","7","8","9"]
+word_list = ["playa","oceano","patineta","zapato","tabla","generador","palma","computadora","televisor","pelicula","fotografia","comida","Patillas","Barceloneta","municipio","Vieques","gobernador","ventana","Aibonito","Culebra"]
+wrong_letter = "_"
+# def get_word():
+#     word_list = ["playa","oceano","patineta","zapato","tabla","generador","palma","computadora","televisor","pelicula","fotografia","comida","Patillas","Barceloneta","municipio","Vieques","gobernador","ventana","Aibonito","Culebra"]
+#     return random.choice(word_list)
 def get_word():
-    word_list = ["playa","oceano","patineta","zapato","tabla","generador","palma","computadora","televisor","pelicula","fotografia","comida","Patillas","Barceloneta","municipio","Vieques","gobernador","ventana","Aibonito","Culebra"]
-    return random.choice(word_list)
+    word =  random.choice(word_list)
+    return word
 
 def get_input():
     
@@ -103,6 +108,7 @@ def get_input():
         if(letter in invalid_caracters):
             print("error")
             continue
+
         used_letters.append(letter)
         return letter
 
@@ -110,8 +116,21 @@ print(get_input())
 
 def print_word():
     temp:str = ""
-    len(word)
-     for (letter in word)
-      :
-      for(matches)
-      {temp:"" or ,letter}
+    # len(word)
+    for letter in my_word:
+        if (letter not in my_word):
+            return wrong_letter
+            continue
+        if (letter in my_word):
+            print(letter)
+            continue
+
+    print(temp)
+
+
+
+my_word = get_word()
+while True:
+    print(steps[0]) 
+    get_input()
+    print_word()

3c6ad4a111dd39748bbc4c7d6473e4f3deae5f37
CSI-Ricardo-Davila <89204914+CSI-Ricardo-Davila@users.noreply.github.com>
2022-03-15 10:33:16 -0400

    Bloque 3 Snake game

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -8,8 +8,9 @@ pygame.display.set_caption('Snake game by Edureka')      #This line of code give
 game_over=False                                          #This line of code creates a booleang variale that is set to False, meaning that the game is not over unitl this variable is true.
 while not game_over:                                     #This line of code is a loop that says that the screen will not go away until the game_over variable is set to True
     for event in pygame.event.get():                     #This line of code runs a forloop over every event that will be in a set list. 
-        print(event)   #prints out all the actions that take place on the screen
- 
+        # print(event)   #prints out all the actions that take place on the screen
+        if event.type==pygame.QUIT:                      #This line of code says that when the program detects an attempt to close the program, it will change the game_over variable to True, and this will end the game.
+            game_over=True                               #This line of code gives the variable 'game_over' a True value, which then causes the game to end.
 
 
 

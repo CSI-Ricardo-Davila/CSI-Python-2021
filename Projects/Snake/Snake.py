3a286dd7d94203b771600a183f25d56a51afd4fa
CSI-Ricardo-Davila <89204914+CSI-Ricardo-Davila@users.noreply.github.com>
2022-03-15 10:09:08 -0400

    Bloque 1 de snake

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,13 @@
+#This line of code imports pygame and the functions that come with it
+import pygame
+# This line of code initializes the game
+pygame.init()
+#This line of code creates the screen that will be used for the game
+dis=pygame.display.set_mode((400,300))
+#This line of code updates the screen
+pygame.display.update()
+#This line of code unitializes the game that has been started
+pygame.quit()
+#This line of code unitializes everything
+quit()
+

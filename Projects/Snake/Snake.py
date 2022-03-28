448b2ebedeabe6402429fab718b065f851f1e9aa
CSI-Ricardo-Davila <89204914+CSI-Ricardo-Davila@users.noreply.github.com>
2022-03-28 10:30:17 -0400

    Bloque 5 snake

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -1,22 +1,57 @@
 import pygame           #This line of code imports pygame and the functions that come with it
 pygame.init()           # This line of code initializes the game
 
-dis=pygame.display.set_mode((400,300))      #This line of code creates the screen that will be used for the game
+dis=pygame.display.set_mode((800,400))      #This line of code creates the screen that will be used for the game
 
 
 
 pygame.display.set_caption('Snake game by Edureka')      #This line of code gives the name 'Snake game by Edureka' to the screen
 
 blue=(0,0,255)#Establishes the snakes color with its RGB number. 
+white = (255, 255, 255)
+black = (0, 0, 0)
 red=(255,0,0)#Establishes the color with its RGB number.
  
 
 game_over=False                                          #This line of code creates a booleang variale that is set to False, meaning that the game is not over unitl this variable is true.
+
+x1 = 300                                                #This line of code sets the starting x coordinate position for the head of the snake 
+y1 = 300                                                #This line of code sets the starting y coordinate position for the head of the snake 
+
+x1_change = 0                                           #This line of code holds the updating values of the x coordinate. 
+y1_change = 0                                           #This line of code holds the updating values of the y coordinate.
+
+clock = pygame.time.Clock()                             #This line of code is to see the time frame while the snake is moving
+
 while not game_over:                                     #This line of code is a loop that says that the screen will not go away until the game_over variable is set to True
     for event in pygame.event.get():                     #This line of code runs a forloop over every event that will be in a set list. 
         # print(event)   #prints out all the actions that take place on the screen
         if event.type==pygame.QUIT:                      #This line of code says that when the program detects an attempt to close the program, it will change the game_over variable to True, and this will end the game.
             game_over=True                               #This line of code gives the variable 'game_over' a True value, which then causes the game to end.
+        if event.type == pygame.KEYDOWN:                 #This line of code uses the functions of KEYDOWN to move the snake
+            if event.key == pygame.K_LEFT:               #This line of code moves the snake to the left.
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:            #This line of code moves the snake to the right.
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:              #This line of code moves the snake up.
+                y1_change = -10
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:            #This line of code moves the snake down.
+                y1_change = 10
+                x1_change = 0
+    
+
+    x1 += x1_change                                     #This line of code holds the updating values of the x coordinate.
+    y1 += y1_change                                     #This line of code holds the updating values of the y coordinate.
+    dis.fill(white)                                     #This line of code says that the display screen will be white
+    pygame.draw.rect(dis, black, [x1, y1, 10, 10])      #This line of code draws a rectangle in the display at the given cordinates
+ 
+    pygame.display.update()                             #This line of code updates the screen
+ 
+    clock.tick(30)                                      #This line of code sets the speed for the snake
+ 
     pygame.draw.rect(dis,blue,[200,150,10,10])           #This line of code draws a rectangle in the display at the given cordinates
     pygame.display.update()                     #This line of code updates the screen
 
